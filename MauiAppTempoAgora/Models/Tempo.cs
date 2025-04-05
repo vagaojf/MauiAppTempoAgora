384c6b507d066c34c1b353e4b825dd119ab3f73f
vagaojf <vagaojf@gmail.com>
2025-04-05 16:02:36 -0300

    cria ção do projeto, models e services.

---
+++ MauiAppTempoAgora/Models/Tempo.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiAppTempoAgora.Models
+{
+    public class Tempo
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+        public int temp_min { get; set; }
+        public int temp_max { get; set; }
+        public int visibility { get; set; }
+        public double speed { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+
+    }
+}
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MauiAppTempoAgora.Models;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo t = null;
+
+            string chave = "15895bdc57f1db9ffc4b9fceea75ec28";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+                }
+
+            }
+
+            return t;
+        }
+    }
+}
